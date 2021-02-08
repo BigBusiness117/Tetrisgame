ab941f6fbbf593095e5fba356cd217f61f30ce29
BigBusiness117 <67163482+BigBusiness117@users.noreply.github.com>
2021-02-08 09:18:16 -0800

    falling Block game
    fixed collision on the falling blocks

---
+++ Assets/mnrty.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mnrty : MonoBehaviour
+{
+    Vector3 move;
+    private GameObject Block;
+    public float speed = -1;
+    private Rigidbody rb;
+    private float downWardSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        move = new Vector3(0, speed, 0);
+        gameObject.transform.Translate(move * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            gameObject.transform.Translate(-downWardSpeed, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            gameObject.transform.Translate(downWardSpeed, 0, 0);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            gameObject.transform.Translate(0, -downWardSpeed, 0);
+
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+
+        move *= -1;
+        Debug.Log("hi");
+
+    }
+}
--- Assets/scripts/BlockSpawn.cs
+++ Assets/scripts/BlockSpawn.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class BlockSpawn : MonoBehaviour
 {
     public GameObject spawn;
-    Vector3 move = new Vector3(0, -1, 0);
+    Vector3 move; 
     private GameObject Block;
+    public float speed = -1; 
     private Rigidbody rb;
+    private float downWardSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,21 +20,31 @@ public class BlockSpawn : MonoBehaviour
         rb = Block.GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotation;
         rb.useGravity = false;
+        Block.AddComponent<BoxCollider>();
+        Block.AddComponent<BlockCollisionDetector>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        move = new Vector3(0, speed, 0);
         Block.transform.Translate(move * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.A))
         {
-            Block.transform.Translate(-1, 0, 0);
+            Block.transform.Translate(-downWardSpeed, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            Block.transform.Translate(1, 0, 0);
+            Block.transform.Translate(downWardSpeed, 0, 0);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Block.transform.Translate(0, -downWardSpeed, 0);
 
         }
 
     }
+
 }
